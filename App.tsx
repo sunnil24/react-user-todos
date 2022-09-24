4edc0ac47a73bca2852fd5670a4068d251f7e5b9
sunnil24 <sunilsri.srivastava3@gmail.com>
2022-09-24 15:58:57 +0530

    Initial commit

---
+++ App.tsx
@@ -0,0 +1,11 @@
+import * as React from 'react';
+import './style.css';
+
+export default function App() {
+  return (
+    <div>
+      <label>Select User</label>
+      <p>Start editing to see some magic happen :)</p>
+    </div>
+  );
+}
---
+++ README.md
@@ -0,0 +1,3 @@
+# react-user-todos
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/react-ts-vuusm7)
